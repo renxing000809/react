21aa98e2d8d62b09647cf0e716eccad7f24c28eb
renxing <373006316@qq.com>
2025-04-06 15:42:57 +0800

    记账本异步请求

--- src/main.jsx
+++ src/main.jsx
@@ -13,9 +13,9 @@ import App from "@/App.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/*<RouterProvider router={router}/>*/}
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router}/>
+      {/*<App />*/}
     </Provider>
   </StrictMode>
 )
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,12 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit";
 import counterStore1 from "@/store/modules/counterStore.jsx";
 import channelStore2 from "@/store/modules/channelStore.jsx";
+import billStore from "@/store/modules/billStore.js";
 
 // 组合多个reducer
 const store = configureStore({
   reducer: {
     counter: counterStore1,
-    channel: channelStore2
+    channel: channelStore2,
+    bill: billStore
   }
 })
 
